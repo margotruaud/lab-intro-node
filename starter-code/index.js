2484529374c019c68d36ffb8d9e0228da5eaacd1
Margot Ruaud <margotruaud@gmail.com>
2019-11-19 20:58:51 +0100

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,44 @@
 class SortedList {
-    constructor() {}
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    constructor() {
+      this.items=[];
+      this.length = this.items.length;
+    }
+    add(item) {
+      this.items.push(item);
+      this.length = this.items.length;
+      this.items.sort(function(a, b) {
+        return a - b;
+      });
+    }
+
+    get(pos) {
+      if (this.length === 0){
+        throw new Error("OutOfBounds");
+      }
+        return(this.items[pos]);
+      }
+
+    max() {
+
+      if (this.length===0){throw new Error("EmptySortedList");}
+      return this.items[this.length - 1 ];
+
+    }
+    min() {
+      if (this.length===0){throw new Error("EmptySortedList");}
+      return this.items[0];
+    }
+    
+    sum() {
+      if (this.length ===0){return 0;}
+      const reducer = (accumulator, currentValue) => accumulator + currentValue;
+      return this.items.reduce(reducer);
+    }
+
+    average() {
+      if (this.length ===0){throw new Error("EmptySortedList");}
+      return this.sum()/this.length;
+    }
   };
   
   module.exports = SortedList;
